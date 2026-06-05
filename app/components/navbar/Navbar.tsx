21a6dd455314904d64fcab8e5f323b574bb5e574
danial137 <danial79fakhrabadi@gmail.com>
2026-06-06 02:31:28 +0330

    Refactor layout and styling; add Navbar component and update global styles

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='w-full fixed top-0 left-0  py-6 border-b bg-white z-10 '>
+            <div className='max-w-[1500px] mx-auto px-6'>
+                asdf
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

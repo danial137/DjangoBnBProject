917b1c9b74d8ce3fcb02f62640b23edb2abd3e52
danial137 <danial79fakhrabadi@gmail.com>
2026-06-06 15:54:39 +0330

    Update Categories component styling for improved hover effects and add beach icon

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -5,7 +5,7 @@ const Categories = () => {
     return (
         <div className='pt-3 cursor-pointer pb-6 flex items-center space-x-12'>
 
-            <div className='pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200'>
+            <div className='pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-800'>
 
                 <Image src="/icn_category_beach.jpeg" alt='beach' width={20} height={20} />
                 <span className='text-xs'>Beach</span>

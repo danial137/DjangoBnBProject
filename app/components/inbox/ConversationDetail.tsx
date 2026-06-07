88984ad0a44ae63e62d0b7094beb886753b0f98d
danial137 <danial79fakhrabadi@gmail.com>
2026-06-07 14:45:45 +0330

    feat: add ConversationDetail component and ConversationPage for inbox functionality

---
+++ app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ConversationDetail = () => {
+    return (
+        <div className='max-h-[400px] overflow-auto flex flex-col space-y-4'>
+
+            <div className='w-[80%] py-4 px-6 rounded-xl bg-gray-200'>
+
+                <p className='font-bold text-gray-500 '>John Doe </p>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio libero quo laborum dolores, incidunt repudiandae eveniet placeat ipsam quos delectus.</p>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ConversationDetail
